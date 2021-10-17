988bd4e318902e7b39be59b5d34360b840924cea
YDT_DOM\dan-gl <dan-gl@yit.co.il>
2021-10-17 13:59:42 +0300

    # impl

--- app/src/main/java/com/mdgd/rustapp/MainActivity.java
+++ app/src/main/java/com/mdgd/rustapp/MainActivity.java
@@ -1,14 +1,27 @@
 package com.mdgd.rustapp;
 
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    static {
+        System.loadLibrary("rust");
+    }
+
+    private native void renderFractal(Bitmap bitmap);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ImageView imageView = findViewById(R.id.imageView);
+        final Bitmap bitmap = Bitmap.createBitmap(800, 800, Bitmap.Config.ARGB_8888);
+        renderFractal(bitmap);
+        imageView.setImageBitmap(bitmap);
     }
 }
